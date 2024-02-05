11a89c12e4ae0d70fbc6d8581835c3d844aee893
Emilio de la Torre <emilio.delatorre@ft.com>
2024-02-05 15:07:43 +0000

    chore: fix spacing on karma Tool Kit plugin

--- toolkit/karma/index.js
+++ toolkit/karma/index.js
@@ -4,13 +4,13 @@ const { hookFork, waitOnExit } = require('@dotcom-tool-kit/logger');
 const karmaCLIPath = require.resolve('karma/bin/karma')
 
 class Karma extends Task {
-    async run() {
-			const args = ['start', 'test/karma.conf.js']
-			this.logger.info(`running karma ${args.join(' ')}`)
-			const child = spawn(karmaCLIPath, args)
-			hookFork(this.logger, 'karma', child)
-			return waitOnExit('karma', child)
-    }
+	async run() {
+		const args = ['start', 'test/karma.conf.js']
+		this.logger.info(`running karma ${args.join(' ')}`)
+		const child = spawn(karmaCLIPath, args)
+		hookFork(this.logger, 'karma', child)
+		return waitOnExit('karma', child)
+	}
 }
 
 exports.tasks = [ Karma ];
